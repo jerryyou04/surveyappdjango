f60f2a55ac3eb6e7d2a0810bc119588c4cff7e65
jerryyou04 <jerryyou04@gmail.com>
2025-03-20 21:26:27 -0400

    Cleaned up old folder and added README

--- myproject1/surveyapp/widgets.py
+++
@@ -1,6 +0,0 @@
-from django_svelte_jsoneditor.widgets import SvelteJSONEditorWidget
-
-class CustomSvelteJSONEditorWidget(SvelteJSONEditorWidget):
-    def __init__(self, attrs=None):
-        # Remove 'style' and add other valid props if necessary
-        super().__init__(attrs)
